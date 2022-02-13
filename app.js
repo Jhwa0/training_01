321bff0d2c8f191131f3570ceca32f23c2f82b62
Jhwa0 <hwayoung0817@naver.com>
2022-02-13 16:21:04 +0900

    상태표시 / 읽은글 표시하기

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
     currentPage:1,
+    feeds: [],
 };
 
 function getData(url){
@@ -14,15 +15,22 @@ function getData(url){
     return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds){
+    for (let i=0; i<feeds.length; i++){
+        feeds[i].read = false;
+    }
+    return feeds;
+}
+
 function newsFeed(){
-    const newsFeed = getData(NEWS_URL);
+    let newsFeed = store.feeds;
     const newsList = [];
     let template = `
         <div class="bg-gray-600 min-h-screen">
             <div class="bg-white text-xl">
                 <div class="mx-auto px-4">
                 <div class="flex justify-between items-center py-6">
-                    <div class="flex justify-start">
+                    <div class="flex justify-start">  
                     <h1 class="font-extrabold">Hacker News</h1>
                     </div>
                     <div class="items-center justify-end">
@@ -42,6 +50,11 @@ function newsFeed(){
         </div>
     `;
     
+
+    if(newsFeed.length === 0 ){
+        newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    }
+
     for(let i = (store.currentPage -1) * 10; i < store.currentPage * 10; i++){
         newsList.push(`
             <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
@@ -102,6 +115,13 @@ function newsDetail(){
         </div>
     </div>
     `;
+
+    for(let i = 0; i < store.feeds.length; i++){
+        if(store.feeds[i].id === Number(id)){
+            store.feeds[i].read = true;
+            break;
+        }
+    }
 
     function makeComment(comments, called = 0){
         const commentString = [];
